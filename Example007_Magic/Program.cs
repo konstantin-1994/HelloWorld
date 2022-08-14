5a3eef484ae4f763ab3c92b84cc7c532d15195f4
Konstantin <cazalova@yandex.rugit config --global user.email cazalova@yandex.ru>
2022-08-14 05:16:50 +0300

    New folders

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10,4);
+//Console.WriteLine("+");
+
+Int32 xa=1, ya= 1,
+    xb=10, yb=10,
+    xc=80, yc=30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
